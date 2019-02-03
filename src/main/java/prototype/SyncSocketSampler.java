cbd1df4d137204b4fce66200cb93db2ff9b55e2a
Kirill Meng <kirill.meng@hotmail.de>
2019-02-03 13:41:19 +0100

    add new gradle and idea configurations

--- src/main/java/prototype/SyncSocketSampler.java
+++ src/main/java/prototype/SyncSocketSampler.java
@@ -1,5 +1,6 @@
 package prototype;
 
+import org.apache.jmeter.config.Arguments;
 import org.apache.jmeter.protocol.java.sampler.AbstractJavaSamplerClient;
 import org.apache.jmeter.protocol.java.sampler.JavaSamplerContext;
 import org.apache.jmeter.samplers.SampleResult;
@@ -7,6 +8,7 @@ import prototype.model.Coordinate;
 import prototype.routing.routeImpl.RectangleRoute;
 
 import java.io.IOException;
+import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.io.Serializable;
 import java.net.InetSocketAddress;
@@ -17,7 +19,7 @@ public class JavaSocketSampler extends AbstractJavaSamplerClient implements Seri
     public SampleResult runTest(JavaSamplerContext javaSamplerContext) {
         SampleResult result = new SampleResult();
         result.sampleStart();
-
+        int elements = javaSamplerContext.getIntParameter("ELEMENTS");
         Socket socket = new Socket();
         try {
             socket.connect(new InetSocketAddress("192.168.0.199", 1337));
@@ -25,16 +27,20 @@ public class JavaSocketSampler extends AbstractJavaSamplerClient implements Seri
             e.printStackTrace();
         }
         ObjectOutputStream oos = null;
+        ObjectInputStream ois = null;
         try {
-            for(Coordinate coordinate: new RectangleRoute().getRouteAsStream()){
+            for(int i = 0; i< elements; ++i){
                 oos = new ObjectOutputStream(socket.getOutputStream());
-                oos.writeObject(coordinate);
+                oos.writeObject(new Coordinate(i,i));
                 oos.flush();
+
+                ois = new ObjectInputStream(socket.getInputStream());
+                Coordinate c = (Coordinate) ois.readObject();
             }
             oos.close();
             socket.close();
             result.setSuccessful(true);
-        } catch (IOException e) {
+        } catch (IOException | ClassNotFoundException e) {
             e.printStackTrace();
             result.setSuccessful(false);
         }
@@ -45,5 +51,12 @@ public class JavaSocketSampler extends AbstractJavaSamplerClient implements Seri
 
     public static void main(final String... args){
         new JavaSocketSampler().runTest(null);
+    }
+
+    @Override
+    public Arguments getDefaultParameters() {
+        Arguments arguments = new Arguments();
+        arguments.addArgument("ELEMENTS", "1");
+        return arguments;
     }
 }
