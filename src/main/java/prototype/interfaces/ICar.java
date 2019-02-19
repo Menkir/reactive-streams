805a1e0816b2eb45403c468b99938d66bd981700
Kirill Meng <kirill.meng@doubleslash.de>
2019-02-19 10:28:36 +0100

    remove unused Interfaces

--- src/main/java/prototype/interfaces/ICar.java
+++
@@ -1,8 +0,0 @@
-package prototype.interfaces;
-
-public interface ICar {
-	void connect();
-	void send();
-	void send(int numberOfData);
-	void close();
-}
--- src/main/java/prototype/interfaces/IServer.java
+++
@@ -1,8 +0,0 @@
-package prototype.interfaces;
-
-import java.io.IOException;
-
-public interface IServer {
-	void receive() throws IOException;
-	void close();
-}
